16c2cd0fbeea324ce04bbee96d70f044703205c6
DiNeez <dneez86@gmail.com>
2025-06-25 19:25:01 +0000

    pagination started

--- src/modules/todo-list/api.ts
+++ src/modules/todo-list/api.ts
@@ -1,5 +1,15 @@
 const BASE_URL = "http://localhost:3000";
 
+export type PaginatedResponse<T> = {
+  data: T[];
+  first: number;
+  last: number;
+  items: number;
+  next: number | null;
+  prev: number | null;
+  pages: number;
+};
+
 export type TodoDto = {
   id: string;
   text: string;
@@ -7,11 +17,19 @@ export type TodoDto = {
 };
 
 export const todoListApi = {
-  getTodos: async ({ signal }: { signal: AbortSignal }) => {
-    const response = await fetch(`${BASE_URL}/todos`, { signal });
+  getTodos: async (
+    { page }: { page: number },
+    { signal }: { signal: AbortSignal },
+  ) => {
+    const response = await fetch(
+      `${BASE_URL}/todos?_page=${page}&_per_page=10`,
+      {
+        signal,
+      },
+    );
     if (!response.ok) {
       throw new Error(`Error fetching todo list: ${response.statusText}`);
     }
-    return response.json() as Promise<TodoDto[]>;
-  }
+    return response.json() as Promise<PaginatedResponse<TodoDto>>;
+  },
 };
--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { todoListApi } from "@/modules/todo-list/api";
 
 export function TodoList() {
-  const { data, error, isPending } = useQuery({
+  const [page, setPage] = useState(1);
+
+  const {
+    data: todoItems,
+    error,
+    isPending,
+  } = useQuery({
     queryKey: ["tasks"],
-    queryFn: todoListApi.getTodos,
+    queryFn: (meta) => todoListApi.getTodos({ page }, meta),
   });
 
   if (isPending) {
@@ -19,7 +26,7 @@ export function TodoList() {
     <div className="mx-auto mt-5 max-w-[1200px] p-5">
       <div className="flex flex-col gap-2">
         <h1 className="text-2xl font-bold">Todo List</h1>
-        {data?.map((task) => (
+        {todoItems.data?.map((task) => (
           <div
             key={task.id}
             className="flex justify-between gap-2 rounded border border-slate-300 px-5 py-4"
@@ -28,6 +35,20 @@ export function TodoList() {
             <input type="checkbox" checked={task.done} readOnly />
           </div>
         ))}
+        <div className="flex gap-2">
+          <button
+            onClick={() => setPage((p) => Math.max(p - 1, 1))}
+            className="rounded border border-teal-500 p-3"
+          >
+            prev
+          </button>
+          <button
+            onClick={() => setPage((p) => Math.min(p + 1, todoItems.pages))}
+            className="rounded border border-teal-500 p-3"
+          >
+            next
+          </button>
+        </div>
       </div>
     </div>
   );
